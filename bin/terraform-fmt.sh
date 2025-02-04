5fbec364bff704d56553fa8cc2fe4518ec40fa55
Philip Crockett <philip.crockett@thermondo.de>
2025-02-04 14:38:13 +0100

    initial draft

---
+++ bin/terraform-fmt.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Remove excessive empty lines and run `terraform fmt` on files
+#
+# Example usage:
+#
+#     ./terraform-fmt.sh foo.tf bar.tf
+#
+
+set -eo pipefail
+
+init() {
+    if command -v gsed &>/dev/null; then
+        SED_CMD="gsed"
+    else
+        SED_CMD="sed"
+    fi
+}
+
+main() {
+    init
+    $SED_CMD -i '/^$/N;/^\n$/D' "$@"
+    terraform fmt "$@"
+}
+
+main "$@"
